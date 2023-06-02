f7ce3bfad38e286d4a332bf90296ca56c3457aa6
Frederico Mattos <fredericomattos25@gmail.com>
2023-06-02 15:54:15 -0300

    add 2nd Problem

---
+++ Transcribing_DNA_to_RNA.py
@@ -0,0 +1,20 @@
+"""
+Problem
+
+An RNA string is a string formed from the alphabet containing 'A', 'C', 'G', and 'U'.
+
+Given a DNA string t
+corresponding to a coding strand, its transcribed RNA string u is formed
+by replacing all occurrences of 'T' in t with 'U' in u
+
+.
+
+Given: A DNA string t having length at most 1000 nt.
+
+Return: The transcribed RNA string of t.
+"""
+import sys
+
+dna_str = open(sys.argv[1]).read()
+
+print(dna_str.replace("T", "U"))
